3b69ddb2ff78e89b3644755cbe154146a0b8b9ee
Beibhinn Keenan <beibhinn.keenan@gmail.com>
2019-05-29 16:07:40 +0000

    Update settings file

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,6 +30,11 @@ ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'),
                  os.environ.get('HOSTNAME')]
 
 
+host = os.environ.get('SITE_HOST')
+if host:
+    ALLOWED_HOSTS.append(host)
+
+
 # Application definition
 
 INSTALLED_APPS = [
