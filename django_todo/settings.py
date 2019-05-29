e21ef625ecb3022aa7e8059f9e9caceed7da920a
Beibhinn Keenan <beibhinn.keenan@gmail.com>
2019-05-29 16:27:31 +0000

    Fix automatic deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,9 +30,6 @@ ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'),
                  os.environ.get('HOSTNAME')]
 
 
-host = os.environ.get('SITE_HOST')
-if host:
-    ALLOWED_HOSTS.append(host)
 
 
 # Application definition
